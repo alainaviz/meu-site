2bd736919c6ffd4bd659bb5d26b3637eec675ceb
Alain <102110747+alainaviz@users.noreply.github.com>
2023-09-04 20:09:23 -0300

    Create script1.js

---
+++ js/script1.js
@@ -0,0 +1,34 @@
+window.onload =function(){
+    findProduc();
+}
+
+function findProduc(){
+
+    let APIKey = "1790900875764";
+    AppSecret = "6ae2a11c15ba1ec55af016908428e8f1"
+    let BaseUrl = "https://app.omie.com.br/api/v1/geral/produtos/";
+
+    var headers = new Headers();
+    headers.append("Content-type", "appication/json");
+
+    const bodyParameters ={
+        call: 'ConsultarProduto',
+        app_key : APIKey,
+        app_secret: AppSecret,
+        param : [{
+            "codigo_produto": 0,
+            codigo_produto_integracao: "",
+            codigo: 242}]
+    }
+
+    const parameters = {method: "POST", headers: headers, body: JSON.stringify(bodyParameters)}
+
+
+    fetch(BaseUrl, parameters)
+    .then(function(response){
+        console.log(response)
+    })
+    .catch(function(err){
+        console.console.error(err);
+    })
+}
